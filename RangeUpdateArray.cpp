1be8d13570fd04abff377022ef57065a3e429a8d
njllucy <116973813+njllucy@users.noreply.github.com>
2025-01-31 23:33:28 +0600

    Add files via upload
    Array Range Update
    
    Given an array a of n integers and also the number of operations q to apply on the array.
    In each operation, you are given a character either ‘+’ or ‘-’, which represents the operation
    type and three integers l, r and v. If the operation type is ‘+’, then you have to add v to all
    the integers from index l to r. And if the operation type is ‘-’, then you have to subtract v
    from all the integers from index l to r. After applying all the operations, print the updated
    array.
    1 <= n <= 5*10^5, -10^6 <= a[i] <=10^6, 1 <= q <= 5*10^5
    1 <= l <=r <= n, -10^6 <= v <= 10^6
    Sample:
    8
    -2 5 3 6 -10 12 4 -3
    2
    + 1 5 6
    - 4 7 -8
    Output:
    4 11 9 4 -12 4 -4 -3

---
+++ RangeUpdateArray.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int a[n+1],fre[n+1]={0};
+  for(int i=1;i<=n;i++){
+    cin>>a[i];
+  }
+  int que,l,r,v;
+  char ch;
+  cin>>que;
+  while(que--){
+    cin>>ch>>l>>r>>v;
+    if(ch=='+'){
+        fre[l]+=v;
+        if(r+1<=n){
+                if(v>0)
+            fre[r+1]-=v;
+        }
+    }
+    else{
+        if(v>0){
+        fre[l]-=v;
+          if(r+1<=n){
+            fre[r+1]+=v;
+        }}else{
+               fre[l]+=v;
+                 if(r+1<=n){
+            fre[r+1]-=v;
+        }
+        }
+    }
+  }
+ for(int i=1;i<=n;i++){
+    fre[i]+=fre[i-1];
+  }
+ for(int i=1;i<=n;i++){
+    a[i]+=fre[i];
+  }
+  for(int i=1;i<=n;i++){
+    cout<<a[i]<<" ";
+  }
+}
